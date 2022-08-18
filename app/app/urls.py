bba3878d4dc8822f0bfd5548fd37f586125eaab7
tabishimam2 <timam799@gmail.com>
2022-08-18 17:08:06 +0530

    minor corrections in admins.py

--- app/app/urls.py
+++ app/app/urls.py
@@ -22,9 +22,9 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/schema',SpectacularAPIView.as_view(), name='api-schema'),
+    path('api/schema/',SpectacularAPIView.as_view(), name='api-schema'),
     path(
-        'api/docs',
+        'api/docs/',
         SpectacularSwaggerView.as_view(url_name ='api-schema' ),
         name='api-docs',
     ),
--- app/core/admin.py
+++ app/core/admin.py
@@ -2,7 +2,6 @@
 Django admin customization
 """
 
-from dataclasses import fields
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
