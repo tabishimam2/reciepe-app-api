279c20d48c1167d988d7a582f68b1a5dc834ee48
tabishimam2 <timam799@gmail.com>
2022-08-18 10:30:55 +0530

    user page customization

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,9 +2,10 @@
 Django admin customization
 """
 
+from dataclasses import fields
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
+from django.utils.translation import gettext_lazy as _
 from core import models
 
 class UserAdmin(BaseUserAdmin):
@@ -12,8 +13,31 @@ class UserAdmin(BaseUserAdmin):
 
     ordering = ['id']
     list_display = ['email','name']
-
-
+    fieldsets= (
+        (None, {'fields': ('email','name')}),
+        (
+            _('Permissions'),
+            {
+                'fields':(
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'),{'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None,{
+            'classes': ('wide',),
+            'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
 admin.site.register(models.User, UserAdmin)
 
 
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -30,9 +30,23 @@ class AdmiSiteTest(TestCase):
     def test_users_list(self):
         """Test that user are listed on page"""
 
-        url = reverse('admin:core_user_changelist')
+        url = reverse('admin:core_user_changelist',)
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test the edit user page work """
+        url = reverse('admin:core_user_change', args = [self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test the  create user page works"""
+        url = reverse('admin:core_user_change')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code , 200)
 
